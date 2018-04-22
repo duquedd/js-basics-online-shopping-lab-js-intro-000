677b5510796cd9c479f27f66311e18b0d9ba3f5f
duquedd1@gmail.com Duque <duquedd1@gmail.com>
2018-04-22 18:34:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function total() {
 function removeFromCart(item) {
 
   for(var i=0; i< cart.length; i++){
-    if (cart[i]['itemName']===item){
+    if (cart[i]['itemName']==item){
       delete cart[i][`itemName`]
       delete cart[i][`itemPrice`]
       return cart
